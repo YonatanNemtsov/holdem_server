7122666d1aeefd6483c82f0f816a4ec81ba4fa27
Yonatan Nemtsov <yonatan.nemtsov@gmail.com>
2022-12-30 21:30:22 +0200

    modified:   game/__pycache__/consumers.cpython-310.pyc 	modified:   game/__pycache__/urls.cpython-310.pyc 	modified:   game/__pycache__/views.cpython-310.pyc 	modified:   game/urls.py 	modified:   game/views.py 	modified:   guessing_game/__pycache__/settings.cpython-310.pyc 	modified:   guessing_game/settings.py

--- game/consumers.py
+++ game/consumers.py
@@ -1,15 +1,19 @@
 from channels.generic.websocket import WebsocketConsumer
 from random import randint
 import json
-from time import sleep
+from asgiref.sync import async_to_sync
+
+
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self,text_data=None):
         print(text_data)
-        i = randint(0,10)
+        i = randint(0,3)
         guess = json.loads(text_data)['message']
         if guess == str(i):
             self.send(json.dumps({'message':'you won!'}))
@@ -17,4 +21,7 @@ class WSConsumer(WebsocketConsumer):
             self.send(json.dumps({'message':'you lose!'}))
 
     
-    
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
