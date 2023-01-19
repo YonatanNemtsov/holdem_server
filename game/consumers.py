3e42fffcc735bd81e823432ba7288a40b10caec6
Yonatan Nemtsov <yonatan.nemtsov@gmail.com>
2023-01-19 14:48:12 +0200

    Nothing

--- game/consumers.py
+++ game/consumers.py
@@ -1,16 +1,14 @@
 
 from random import randint
-import json
-
-from asgiref.sync import async_to_sync
 from channels.auth import login
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
-
 from .models import GameLog, GameTable
 
 
 
 
 class GameConsumer(AsyncJsonWebsocketConsumer):
-    pass
+    async def connect(self):
+        pass
+    
